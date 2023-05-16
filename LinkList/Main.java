d22d959392ab5b42db8dd1682447be3e796500c4
Miranaq <113391061+Miranaq@users.noreply.github.com>
2023-05-16 12:46:52 +0300

    Update Main.java

--- LinkList/Main.java
+++ LinkList/Main.java
@@ -2,12 +2,12 @@ package LinkList;
 import java.util.function.Predicate;
 public class Main {
     public static void main(String[] args) {
-        MyListable myListable = new MyList();
-        myListable.add(0);
-        myListable.add(2);
-        myListable.add(1,1);
-        myListable.add(4);
-        myListable.remove(3);
-        System.out.println(myListable);
+        AfL afL = new AfL();
+        afL.add(0);
+        afL.add(2);
+        afL.add(1,1);
+        afL.add(4);
+        afL.remove(3);
+        System.out.println(AvailableforListening);
     }
 }
